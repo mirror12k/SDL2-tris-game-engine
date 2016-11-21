6869601556f74e0fdbf6c3d2344044969d85afdf
mirror <mirror12k@gmail.com>
2016-11-20 23:17:27 -0500

    - verified that rect redrawing is working  - implemented an fps ticker entity  - created a neat tech demo to go with it

--- main.cpp
+++ main.cpp
@@ -5,6 +5,10 @@
 #include "tris/engine.hpp"
 
 
+#include <cmath>
+const double pi = std::acos(-1);
+
+
 class my_entity : public tris::entity
 {
 public:
@@ -19,10 +23,10 @@ public:
     virtual void update (tris::engine* eng)
     {
         this->my_box.rect.x += this->sx;
-        if (this->my_box.rect.x >= eng->graphics.window_width || this->my_box.rect.x < 0)
+        if (this->my_box.rect.x + 25 >= eng->graphics.window_width || this->my_box.rect.x + 25 < 0)
             this->sx = -this->sx;
         this->my_box.rect.y += this->sy;
-        if (this->my_box.rect.y >= eng->graphics.window_height || this->my_box.rect.y < 0)
+        if (this->my_box.rect.y + 25 >= eng->graphics.window_height || this->my_box.rect.y + 25 < 0)
             this->sy = -this->sy;
         this->my_box.angle += 0.5;
     }
@@ -33,15 +37,63 @@ public:
     }
 };
 
+class my_invis : public my_entity
+{
+public:
+    int step_a;
+
+    my_invis(int x, int y, int sx, int sy, int step_angle, int step_a)
+    : my_entity(x, y, sx, sy), step_a(step_a)
+    {
+        this->my_box.angle = step_angle;
+    }
+
+    virtual void update (tris::engine* eng)
+    {
+        my_entity::update(eng);
+        this->my_box.rgba.a = 128 + 127 * std::sin(pi * (this->step_a / 180.0));
+        this->my_box.changed = true;
+
+        this->step_a = (this->step_a + 8) % 360;
+    }
+};
+
+class fps_ticker_entity : public tris::entity
+{
+public:
+    uint tick = 0;
+    uint frame = 0;
+
+    virtual void update(tris::engine* eng)
+    {
+        if (this->tick == time(nullptr))
+            this->frame++;
+        else
+        {
+            printf("fps: %d\n", this->frame);
+            this->tick = time(nullptr);
+            this->frame = 0;
+        }
+    }
+};
+
 
 int main ()
 {
+
+    srand(time(nullptr));
     printf("hello world\n");
     tris::engine my_game("test engine", 800, 640, 60);
 //    my_game.graphics.add_box(new tris::box(50, 100, 50, 50, 128, 0, 128, 255));
-    my_game.update_ctx.add_entity(new my_entity(100, 100, 1, 1));
-    my_game.update_ctx.add_entity(new my_entity(100, 100, 1, 2));
-    my_game.update_ctx.add_entity(new my_entity(100, 100, 3, 1));
+//    my_game.update_ctx.add_entity(new my_invis(100, 100, 1, 1, 100));
+//    my_game.update_ctx.add_entity(new my_invis(100, 100, 1, 2, 0));
+//    my_game.update_ctx.add_entity(new my_entity(100, 100, 3, 1));
+//    my_game.update_ctx.add_entity(new my_entity(100, 100, 1, 3));
+    my_game.update_ctx.add_entity(new fps_ticker_entity());
+
+    for (int i = 0; i < 400; i++)
+        my_game.update_ctx.add_entity(new my_invis(rand() % my_game.graphics.window_width, rand() % my_game.graphics.window_height,
+                1 + rand() % 5, 1 + rand() % 5, rand() % 360, 1 + rand() % 360));
     my_game.run();
 
     return 0;
