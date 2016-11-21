433366d5def22dd119d421fbc6b1f73a51cc0f67
mirror <mirror12k@gmail.com>
2016-11-21 13:17:23 -0500

    - implemented a striped texture box to display the extensible renderer

--- main.cpp
+++ main.cpp
@@ -7,15 +7,63 @@
 
 
 
+
+
+class striped_box : public tris::box
+{
+public:
+    int stripe_width;
+    tris::color stripe_rgba;
+    striped_box(int w, int h, int x, int y, tris::color rgba, int stripe_width, tris::color stripe_rgba)
+    : box(w,h,x,y,rgba), stripe_width(stripe_width), stripe_rgba(stripe_rgba)
+    {}
+
+    void render(SDL_Renderer* renderer)
+    {
+        SDL_Texture* stripe = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_TARGET, this->rect.w * 2, this->stripe_width);
+        SDL_SetRenderTarget(renderer, stripe);
+        SDL_SetRenderDrawColor(renderer, this->stripe_rgba.r, this->stripe_rgba.g, this->stripe_rgba.b, this->stripe_rgba.a);
+        SDL_RenderFillRect(renderer, nullptr);
+
+        this->tex = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_TARGET, this->rect.w, this->rect.h);
+        SDL_SetRenderTarget(renderer, this->tex);
+        SDL_SetRenderDrawColor(renderer, this->rgba.r, this->rgba.g, this->rgba.b, this->rgba.a);
+        SDL_RenderFillRect(renderer, nullptr);
+
+        SDL_Rect dest;
+        dest.w = this->rect.w * 2;
+        dest.h = this->stripe_width;
+        for (int offset = 0; offset < this->rect.h; offset += this->stripe_width * 1.414)
+        {
+            dest.x = -offset;
+            dest.y = offset;
+            SDL_RenderCopyEx(renderer, stripe, nullptr, &dest, 45, nullptr, SDL_FLIP_NONE);
+        }
+
+        if (this->rgba.a == 255)
+            SDL_SetTextureBlendMode(this->tex, SDL_BLENDMODE_NONE);
+        else
+            SDL_SetTextureBlendMode(this->tex, SDL_BLENDMODE_BLEND);
+
+        SDL_SetRenderTarget(renderer, nullptr);
+    }
+};
+
+
+
+
+
 class my_entity : public tris::entity
 {
 public:
-    tris::box my_box;
+    striped_box my_box;
 
     int sx, sy;
 
     my_entity(int x, int y, int sx, int sy)
-    : my_box(100, 100, x-25, y-25, 128, 255, 128, 255), sx(sx), sy(sy)
+//    : my_box(200, 200, x-25, y-25, 128, 255, 128, 255),
+    : my_box(200, 200, x-25, y-25, tris::color(128, 255, 128, 255), 20, tris::color(128, 128, 255, 255)),
+    sx(sx), sy(sy)
     {}
 
     virtual void update (tris::engine* eng)
@@ -71,11 +119,14 @@ int main ()
 //    my_game.update_ctx.add_entity(new my_entity(100, 100, 3, 1));
 //    my_game.update_ctx.add_entity(new my_entity(100, 100, 1, 3));
     my_game.update_ctx.add_entity(new tris::util::fps_ticker_entity());
-    my_game.update_ctx.add_entity(new tris::util::frame_benchmark_entity(300));
+//    my_game.update_ctx.add_entity(new tris::util::frame_benchmark_entity(300));
 
-    for (int i = 0; i < 1000; i++)
-        my_game.update_ctx.add_entity(new my_invis(rand() % my_game.graphics.window_width, rand() % my_game.graphics.window_height,
-                1 + rand() % 5, 1 + rand() % 5, rand() % 360, 1 + rand() % 360));
+//    for (int i = 0; i < 1000; i++)
+//        my_game.update_ctx.add_entity(new my_invis(rand() % my_game.graphics.window_width, rand() % my_game.graphics.window_height,
+//                1 + rand() % 5, 1 + rand() % 5, rand() % 360, 1 + rand() % 360));
+    for (int i = 0; i < 10; i++)
+        my_game.update_ctx.add_entity(new my_entity(rand() % my_game.graphics.window_width, rand() % my_game.graphics.window_height,
+                1 + rand() % 5, 1 + rand() % 5));
     my_game.run();
 
     return 0;
--- tris/box.hpp
+++ tris/box.hpp
@@ -14,7 +14,7 @@ struct color
     color(uint8_t r, uint8_t g, uint8_t b, uint8_t a);
 };
 
-struct box
+class box
 {
 public:
     bool changed = false;
@@ -29,7 +29,7 @@ public:
     box(int w, int h, int x, int y, color rgba);
     ~box();
 
-    void render(SDL_Renderer* renderer);
+    virtual void render(SDL_Renderer* renderer);
 };
 
 }
